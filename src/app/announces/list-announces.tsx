c9833a8090827ed6614eed55447776476d90ee74
Redouane Bouabana <rredouane342@gmail.com>
2023-05-11 23:27:06 +0100

    add smooth animation when the listing is refetched

--- src/app/announces/list-announces.tsx
+++ src/app/announces/list-announces.tsx
@@ -6,14 +6,17 @@ import SideFilter from "@/components/main/filters/SideFilter";
 import SortFilter from "@/components/main/filters/SortFilter";
 import getData from "@/helpers/getData";
 import { useQuery } from "@tanstack/react-query";
+import Image from "next/image";
 import { useSearchParams } from "next/navigation";
 import React from "react";
+import Logo from "@/assets/logo/logo_ocarz.png";
+import { Transition } from "@headlessui/react";
+import CreateAlert from "@/components/main/announceSection/card/newAlert";
 
 export default function ListAnnounces() {
   const searchParams = useSearchParams();
   const strParams = searchParams.toString();
-  const [filter, setFilter] = React.useState("");
-  const [sort, setSort] = React.useState(searchParams.get("sort") || "asc");
+  const [sort, setSort] = React.useState("");
   const [managed, setManaged] = React.useState("");
   const { data, isLoading, isFetching, error, refetch } = useQuery({
     queryKey: ["announces"],
@@ -33,7 +36,37 @@ export default function ListAnnounces() {
       <div className="col-span-4 bg-white p-4">
         <DisclaimerCard />
         <SortFilter setManaged={setManaged} setSort={setSort} />
-        {isLoading ? "loading" : <AnnounceSection productsData={data} />}
+        {isLoading || isFetching ? (
+          <div className="flex justify-center w-full">
+            <Image
+              loader={({ src }) => src}
+              src={Logo}
+              className="animate-pulse bg-primary p-3 m-10"
+              width={200}
+              height={200}
+              alt=""
+            />
+          </div>
+        ) : (
+          <Transition
+            show={!isLoading}
+            enter="transition-opacity duration-75"
+            enterFrom="opacity-0"
+            enterTo="opacity-100"
+            leave="transition-opacity duration-150"
+            leaveFrom="opacity-100"
+            leaveTo="opacity-0"
+          >
+            <AnnounceSection productsData={data} />
+          </Transition>
+        )}
+        <div className="my-5">
+          {/* <Pagination totalProduct={totalProduct}/> */}
+        </div>
+
+        <div className="my-5">
+          <CreateAlert />
+        </div>
       </div>
     </div>
   );
--- src/components/main/announceSection/main.tsx
+++ src/components/main/announceSection/main.tsx
@@ -9,20 +9,13 @@ import { Fragment } from "react";
 
 export default function AnnounceSection({ productsData }: any) {
   return (
-    <div className="mb-6">
+    <div key={productsData} className="mb-6 anime-enter-active">
       <div className="flex flex-wrap gap-3">
         {productsData?.map((product: any) => (
           <Fragment key={product.id}>
             <AnnounceCard product={product} />
           </Fragment>
         ))}
-      </div>
-      <div className="my-5">
-        {/* <Pagination totalProduct={totalProduct}/> */}
-      </div>
-
-      <div className="my-5">
-        <CreateAlert />
       </div>
     </div>
   );
